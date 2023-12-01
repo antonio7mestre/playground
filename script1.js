ab226356c161517a9d710d09037bf595f7acf4c1
Antonio Mestre <antonio7mestre@gmail.com>
2023-11-30 22:59:21 -0500

    “bigcheckpoint”

--- script1.js
+++ script1.js
@@ -10,16 +10,16 @@ const audioFiles = {
     "checkpoint4": "audio/audio4.mp3",
     "checkpoint5": "audio/audio5.mp3",
     "checkpoint6": "audio/audio6.mp3",
-    "checkpoint7": "audio/audio7.mp3"
+    "checkpoint7": "audio/silent.mp3"
 };
 const checkpoints = [
-    { lat: 38.890760751698224, lng: -77.09456107931626, radius: 30, audioKey: "checkpoint1" }, // Example coordinates and radius
-    { lat: 38.89095162561316, lng: -77.09519500334783, radius: 30, audioKey: "checkpoint2" },
-    { lat: 38.89139152494132, lng: -77.09419741012081, radius: 30, audioKey: "checkpoint3" },
-    { lat: 38.891828469221075, lng: -77.09312395428827, radius: 30, audioKey: "checkpoint4" },
-    { lat: 38.89112581426466, lng: -77.09258153669099, radius: 30, audioKey: "checkpoint5" },
-    { lat: 38.89017217737917, lng:  -77.09202687325174, radius: 30, audioKey: "checkpoint6" },
-    { lat: 38.89029330568399, lng: -77.09354926107771, radius: 30, audioKey: "checkpoint7" }
+    { lat: 38.890760751698224, lng: -77.09456107931626, radius: 15, audioKey: "checkpoint1" }, // Example coordinates and radius
+    { lat: 38.89095162561316, lng: -77.09519500334783, radius: 15, audioKey: "checkpoint2" },
+    { lat: 38.89139152494132, lng: -77.09419741012081, radius: 15, audioKey: "checkpoint3" },
+    { lat: 38.891828469221075, lng: -77.09312395428827, radius: 15, audioKey: "checkpoint4" },
+    { lat: 38.89112581426466, lng: -77.09258153669099, radius: 15, audioKey: "checkpoint5" },
+    { lat: 38.89017217737917, lng:  -77.09202687325174, radius: 15, audioKey: "checkpoint6" },
+    { lat: 38.89029330568399, lng: -77.09354926107771, radius: 800, audioKey: "checkpoint7" }
 ];
 
 document.getElementById("startButton").addEventListener("click", function() {
